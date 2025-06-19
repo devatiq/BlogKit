0bc785c1b1572462fcb86ca300e3b2aebaf7ee4c
hmbashar <hmbashar@gmail.com>
2025-06-19 11:54:28 +0600

    styles controls

--- Frontend/Elementor/Widgets/FeaturedSidebar/Main.php
+++ Frontend/Elementor/Widgets/FeaturedSidebar/Main.php
@@ -56,8 +56,94 @@ class Main extends Widget_Base
             ]
         );
 
+        $this->end_controls_section();
 
-        $this->end_controls_section(); 
+
+        // === Featured Section Style ===
+        $this->start_controls_section(
+            'blogkit_fs_style_featured',
+            [
+                'label' => esc_html__('Featured Box', 'blogkit'),
+                'tab' => Controls_Manager::TAB_STYLE,
+            ]
+        );
+
+        $this->add_control(
+            'blogkit_fs_featured_bg',
+            [
+                'label' => esc_html__('Background Color', 'blogkit'),
+                'type' => Controls_Manager::COLOR,
+                'selectors' => [
+                    '{{WRAPPER}} .blogkit-fs-featured' => 'background-color: {{VALUE}};',
+                ],
+            ]
+        );
+
+        $this->add_responsive_control(
+            'blogkit_fs_featured_padding',
+            [
+                'label' => esc_html__('Padding', 'blogkit'),
+                'type' => Controls_Manager::DIMENSIONS,
+                'size_units' => ['px', '%', 'em'],
+                'selectors' => [
+                    '{{WRAPPER}} .blogkit-fs-featured' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+                ],
+            ]
+        );
+
+        $this->add_group_control(
+            \Elementor\Group_Control_Typography::get_type(),
+            [
+                'name' => 'blogkit_fs_featured_title_typo',
+                'selector' => '{{WRAPPER}} .blogkit-fs-title a',
+            ]
+        );
+
+        $this->end_controls_section();
+
+        // === Sidebar Section Style ===
+        $this->start_controls_section(
+            'blogkit_fs_style_sidebar',
+            [
+                'label' => esc_html__('Sidebar Posts', 'blogkit'),
+                'tab' => Controls_Manager::TAB_STYLE,
+            ]
+        );
+
+        $this->add_control(
+            'blogkit_fs_sidebar_bg',
+            [
+                'label' => esc_html__('Item Background', 'blogkit'),
+                'type' => Controls_Manager::COLOR,
+                'selectors' => [
+                    '{{WRAPPER}} .blogkit-fs-sidebar-item' => 'background-color: {{VALUE}};',
+                ],
+            ]
+        );
+
+        $this->add_responsive_control(
+            'blogkit_fs_sidebar_padding',
+            [
+                'label' => esc_html__('Item Padding', 'blogkit'),
+                'type' => Controls_Manager::DIMENSIONS,
+                'size_units' => ['px', '%', 'em'],
+                'selectors' => [
+                    '{{WRAPPER}} .blogkit-fs-sidebar-item' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+                ],
+            ]
+        );
+
+        $this->add_group_control(
+            \Elementor\Group_Control_Typography::get_type(),
+            [
+                'name' => 'blogkit_fs_sidebar_title_typo',
+                'selector' => '{{WRAPPER}} .blogkit-fs-sidebar-title a',
+            ]
+        );
+
+        $this->end_controls_section();
+
+
 
     }
 
